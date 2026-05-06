d23bba6c4c92c2efaea076a2f68e39724e23af7c
Jeremy Cloarec <jeremy.cloarec@filigran.io>
2026-05-06 16:28:28 +0200

    [backend] add engine health monitor (#15860)

--- opencti-platform/opencti-graphql/src/boot.js
+++ opencti-platform/opencti-graphql/src/boot.js
@@ -5,6 +5,7 @@ import { shutdownRedisClients } from './database/redis';
 import { shutdownModules, startModules } from './managers';
 import { initLockFork } from './lock/master-lock';
 import { checkSystemDependencies } from './boot-utils';
+import { startEngineHealthMonitor, stopEngineHealthMonitor } from './database/engine';
 
 // region platform start and stop
 export const platformStart = async () => {
@@ -47,6 +48,8 @@ export const platformStart = async () => {
       logApp.error('[OPENCTI] Modules startup failed', { cause: modulesError });
       throw modulesError;
     }
+    // Start the engine health monitoring CRON
+      startEngineHealthMonitor();
     logApp.info(`[OPENCTI] Platform started ${Date.now() - startTime} ms`);
   } catch (_mainError) {
     process.exit(1);
@@ -55,6 +58,8 @@ export const platformStart = async () => {
 
 export const platformStop = async () => {
   const stopTime = new Date().getTime();
+  // Stop the engine health monitoring CRON
+  stopEngineHealthMonitor();
   // Shutdown the cache manager
   await cacheManager.shutdown();
   // Destroy the modules
